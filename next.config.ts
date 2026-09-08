0c2cee9403a807a016b2910da4b94523d80f4a4c
ibondarc <ibondarc@vm-mashin.snake.io.com>
2026-09-08 13:55:12 +0200

    remove standalone option

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = { 
-    output: 'standalone',  
     allowedDevOrigins:process.env.NEXTAUTH_URL
         ? [new URL(process.env.NEXTAUTH_URL).host]
         : undefined,
